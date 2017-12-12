8df26da5dd8a0b9076e7a56373894ab15e77333e
Rubens Takauti <rtakauti@studiovisual.com.br>
2017-12-12 14:58:39 -0200

    Refatora rotas

--- app/core/routes.php
+++ app/core/routes.php
@@ -7,6 +7,7 @@ use StudioVisual\Core\Route;
 $route = new Route();
 
 $route->get('/', HomeController::class, 'index');
+$route->get('/home', HomeController::class, 'index');
 
 $route->get('/contact', ContactController::class, 'index');
 
